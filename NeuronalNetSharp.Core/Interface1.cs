10167f6f5693e51c523d5be0ae1871306231fa02
Florian <florian.sestak@gmail.com>
2016-08-15 18:44:27 +0200

    Add documents

---
+++ NeuronalNetSharp.Core/Interface1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NeuronalNetSharp.Core
+{
+    public interface Interface1
+    {
+        NeuronalNetwork.NeuronalNetwork NeuronalNetwork { get; set; }
+    }
+}
